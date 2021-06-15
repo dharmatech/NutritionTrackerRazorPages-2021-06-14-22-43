63df388007ab82083c262a0d9426ffdbf232099c
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-06-14 23:03:02 -0700

    Register.cshtml.cs - Username

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,6 +46,11 @@ namespace NutritionTrackerRazorPages.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "User Name")]
+            public string UserName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -74,7 +79,7 @@ namespace NutritionTrackerRazorPages.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.UserName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
@@ -112,3 +117,5 @@ namespace NutritionTrackerRazorPages.Areas.Identity.Pages.Account
         }
     }
 }
+
+
