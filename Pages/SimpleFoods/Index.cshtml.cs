8e34b2c48ce10ff3b5b701c88c450c1acef6c1c8
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-06-20 14:55:56 -0700

    SimpleFoods\Index - ordering

--- Pages/SimpleFoods/Index.cshtml.cs
+++ Pages/SimpleFoods/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -12,21 +13,38 @@ namespace NutritionTrackerRazorPages.Pages.SimpleFoods
 {
     public class IndexModel : PageModel
     {
-        private readonly NutritionTrackerRazorPages.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext      _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public IndexModel(NutritionTrackerRazorPages.Data.ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IList<SimpleFood> SimpleFood { get;set; }
+        public IList<SimpleFood> SimpleFoods { get;set; }
 
         public async Task OnGetAsync()
         {
-            SimpleFood = await _context.SimpleFood
+            var foods = _context.SimpleFood
                 .Include(simpleFood => simpleFood.User)
-                .Include(s => s.FoodCategory)
-                .Include(s => s.User).ToListAsync();
+                .Include(simpleFood => simpleFood.FoodCategory)
+                .AsEnumerable();
+                        
+            var foods_user = foods
+                .Where(simpleFood => simpleFood.User.UserName == _userManager.GetUserName(User))
+                .OrderBy(simpleFood => simpleFood.Name);
+                        
+            var foods_admin = foods
+                .Where(simpleFood => simpleFood.UserId == _userManager.FindByNameAsync("admin").Result.Id)
+                .OrderBy(simpleFood => simpleFood.Name);
+
+            var foods_others = foods.Except(foods_user).Except(foods_admin);
+
+            SimpleFoods = foods_user.Concat(foods_admin)
+                .Concat(foods_others)
+                .ToList();
         }
+
     }
 }
