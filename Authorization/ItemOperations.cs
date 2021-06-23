71e3d5a086dd67e5627f7e976eccfdaeca930f51
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-06-22 17:06:01 -0700

    Authorization\ItemOperation.cs

---
+++ Authorization/ItemOperations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionTrackerRazorPages.Authorization
+{
+    public class Constants
+    {
+        public static readonly string DeleteOperationName = "Delete";
+    }
+
+    public static class ItemOperations
+    {
+        public static OperationAuthorizationRequirement Delete =
+            new OperationAuthorizationRequirement()
+            {
+                Name = Constants.DeleteOperationName
+            };
+    }
+}
