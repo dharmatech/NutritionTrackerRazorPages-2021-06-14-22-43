559d493304a54082dd43073e990e964ce3170d4c
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-06-22 17:11:23 -0700

    Models\IUserOwned.cs

---
+++ Models/IUserOwned.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionTrackerRazorPages.Models
+{
+    public interface IUserOwned
+    {
+        public string UserId { get; set; }
+                
+        public IdentityUser User { get; set; } // navigation
+    }
+}
